9f4445877d2aa9a59f0a8b1349ce1bf8b8021a48
ravisihare123 <ravisihare123@gmail.com>
2022-07-29 17:21:22 +0530

    jwt backend has completed

--- controller/changepassword.js
+++ controller/changepassword.js
@@ -3,23 +3,25 @@ const dbConfig = require("../database/dbConfig");
 async function ChangePassword(req, res){
 
     const{newpassword, confrimpassword} = req.body;
-
+    // console.log(req.body);
     if(newpassword == confrimpassword){
         var result = await dbConfig('changepassword').insert({
             change_pass:req.body.changepassword,
             new_pass:newpassword,
             confrim_pass:confrimpassword
         })
+        // console.log(result);
+        
         if(result){
             res.status(200).json({status:true, data: result,message:"success!!!"})
         }
-        else{
+        else{ 
             console.log(req.body);
             res.status(500).json({status:false,data:false,message:" Failed"})
         }
     }
     else{
-        res.status(200).json({status:false,message:"password dont matched!!!"})
+        res.status(200).json({status:false,message:"password doesn't matched!!!"})
     }
  
 
--- helpers/basehelper.js
+++ helpers/basehelper.js
@@ -5,7 +5,7 @@ module.exports = async (req, res, next) => {
     try {
         const token = req.headers.authorization.split(" ")[1];
         const decoded = await jwt.verify(token,config.secret);
-        console.log(decoded);
+        // console.log(decoded);
         req.userData = decoded;
         next();
     } catch (error) {
